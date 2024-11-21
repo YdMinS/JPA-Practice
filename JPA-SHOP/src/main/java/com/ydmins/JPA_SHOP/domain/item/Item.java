006970cdfc94a9db4a399c72f13af176bdf60c03
ydmins <dr.ydmins@gmail.com>
2024-11-21 15:09:53 +0900

    feature : Add two methods to Item entity class

--- JPA-SHOP/src/main/java/com/ydmins/JPA_SHOP/domain/item/Item.java
+++ JPA-SHOP/src/main/java/com/ydmins/JPA_SHOP/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.ydmins.JPA_SHOP.domain.item;
 
 import com.ydmins.JPA_SHOP.domain.Category;
+import com.ydmins.JPA_SHOP.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,4 +27,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // Business Logic
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity -= quantity;
+    }
 }
