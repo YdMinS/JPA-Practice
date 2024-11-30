fb7a5f9670f867f0f415e7c43ee56c692e57a21f
ydmins <dr.ydmins@gmail.com>
2024-11-30 19:26:35 +0900

    init new project data-jpa

---
+++ data-jpa/src/test/java/com/ydmins/data_jpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydmins.data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
