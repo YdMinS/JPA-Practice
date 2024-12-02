2a8503c7af205020bc9d5233062edfe02e2c751d
ydmins <dr.ydmins@gmail.com>
2024-12-02 18:49:50 +0900

    feat : add TeamRepository with JPA applied

---
+++ data-jpa/src/main/java/com/ydmins/data_jpa/repository/TeamRepository.java
@@ -0,0 +1,7 @@
+package com.ydmins.data_jpa.repository;
+
+import com.ydmins.data_jpa.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
