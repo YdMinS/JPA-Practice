2b20f1a8ce9ec5fb099a9adfaabc1097827ae92a
ydmins <dr.ydmins@gmail.com>
2024-12-04 21:25:10 +0900

    feat : applied with BaseEntity

--- data-jpa/src/main/java/com/ydmins/data_jpa/DataJpaApplication.java
+++ data-jpa/src/main/java/com/ydmins/data_jpa/DataJpaApplication.java
@@ -2,6 +2,11 @@ package com.ydmins.data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 public class DataJpaApplication {
@@ -10,4 +15,10 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider(){
+		// Spring Security에서 사용자 정보를 불러와 byWho 자리에 넣어주면 된다.
+		String byWho = UUID.randomUUID().toString();
+		return () -> Optional.of(byWho);
+	}
 }
