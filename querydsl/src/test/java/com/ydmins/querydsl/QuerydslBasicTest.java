64c5d97ae0561c3f45229a3bba05d274a7385857
ydmins <dr.ydmins@gmail.com>
2024-12-06 16:25:01 +0900

    tested aggregation queries

--- querydsl/src/test/java/com/ydmins/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/com/ydmins/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package com.ydmins.querydsl;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ydmins.querydsl.entity.Member;
 import com.ydmins.querydsl.entity.Team;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static com.ydmins.querydsl.entity.QMember.*;
 import static org.assertj.core.api.Assertions.*;
@@ -64,5 +67,38 @@ public class QuerydslBasicTest {
                     .where(member.username.eq("member1"))
                     .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @DisplayName("QueryDSL : 멤버를 검색한다.")
+    @Test
+    public void withQueryDSL2(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @DisplayName("QueryDSL : 집합 쿼리를 실행한다.")
+    @Test
+    public void withQueryDSL3(){
+        List<Tuple> result = queryFactory
+                .select(
+                    member.count(),
+                    member.age.sum(),
+                    member.age.avg(),
+                    member.age.max(),
+                    member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
     }
 }
