43caecb4712ea1f8ba6ec13f99c0ce66c6a567f8
ydmins <dr.ydmins@gmail.com>
2024-12-16 12:55:51 +0900

    Implement Paging using Spring Data JPA

--- querydsl/src/test/java/com/ydmins/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/com/ydmins/querydsl/repository/MemberRepositoryTest.java
@@ -1,11 +1,16 @@
 package com.ydmins.querydsl.repository;
 
+import com.ydmins.querydsl.dto.MemberSearchCondition;
+import com.ydmins.querydsl.dto.MemberTeamDto;
 import com.ydmins.querydsl.entity.Member;
+import com.ydmins.querydsl.entity.Team;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -35,5 +40,58 @@ public class MemberRepositoryTest {
 
         List<Member> result2 = memberRepository.findByUsername("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @DisplayName("Spring Data JPA : Builder 없이 동적쿼리를 처리한다.")
+    @Test
+    public void test2(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition memberSearchCondition = new MemberSearchCondition();
+        memberSearchCondition.setAgeGoe(35);
+        memberSearchCondition.setAgeLoe(41);
+        memberSearchCondition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberRepository.search(memberSearchCondition);
+        assertThat(result).extracting("username").containsExactly("member4");
+    }
+
+    @DisplayName("Spring Data JPA : Simple Paging")
+    @Test
+    public void test3(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition memberSearchCondition = new MemberSearchCondition();
+        PageRequest pageRequest = PageRequest.of(0, 3);
+
+        Page<MemberTeamDto> result = memberRepository.searchPageSimple(memberSearchCondition, pageRequest);
+        assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
     }
 }
