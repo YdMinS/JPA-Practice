da1f2bee34d99f33263cd6107f7f733cbf5c808c
ydmins <dr.ydmins@gmail.com>
2024-12-16 11:51:45 +0900

    added MemberRepository applied with Spring Data JPA

---
+++ querydsl/src/main/java/com/ydmins/querydsl/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.ydmins.querydsl.repository;
+
+import com.ydmins.querydsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
+}
---
+++ querydsl/src/test/java/com/ydmins/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.ydmins.querydsl.repository;
+
+import com.ydmins.querydsl.entity.Member;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired MemberRepository memberRepository;
+
+    @DisplayName("Spring Data JPA로 Member 엔티티가 저장된다.")
+    @Test
+    public void test1(){
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> result1 = memberRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+    }
+}
